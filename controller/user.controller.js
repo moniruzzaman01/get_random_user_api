04b2de76ec15f5e58c5e49367e9cfdd4ca72f1d0
MONIRUZZAMAN <moniruzzaman@MONIRUZZAMANs-MacBook-Air.local>
2022-09-11 19:03:56 +0600

    save a new user implimented

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,41 +1,48 @@
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
 
-module.exports.getRandomUser=(req,res,next)=>{
-    fs.readFile('data.json',(err,data)=>{
-        if(err)
-        {
-            res.send('Error occured while fetching random data!')
-        }
-        else{
-            const allData=JSON.parse(data)
-            const idArray=allData.map(d=>d.id)
-            const randomNumber = Math.floor(Math.random() * (idArray.length))
-            const randomData= allData.find(d=>d.id===idArray[randomNumber])
-            res.send(randomData)
-        }
-    })
-}
-module.exports.getAllUser=(req,res,next)=>{
-    fs.readFile('data.json',(err,data)=>{
-        if(err)
-        {
-            res.send("Error occured while fetching all data!")
-        }
-    else{
-        res.send(data)
+module.exports.getRandomUser = (req, res, next) => {
+  fs.readFile("data.json", (err, data) => {
+    if (err) {
+      res.send("Error occured while fetching random data!");
+    } else {
+      const allData = JSON.parse(data);
+      const idArray = allData.map((d) => d.id);
+      const randomNumber = Math.floor(Math.random() * idArray.length);
+      const randomData = allData.find((d) => d.id === idArray[randomNumber]);
+      res.send(randomData);
     }
-    })
-}
-module.exports.saveUser=(req,res,next)=>{
-    res.send('Save a user!')
-}
-module.exports.updateUser=(req,res,next)=>{
-    res.send('User updated!')
-}
-module.exports.updateMultipleUser=(req,res,next)=>{
-    res.send('Updated multiple user!')
-}
-module.exports.deleteUser=(req,res,next)=>{
-    res.send('Delete a user!')
-}
-
+  });
+};
+module.exports.getAllUser = (req, res, next) => {
+  fs.readFile("data.json", (err, data) => {
+    if (err) {
+      res.send("Error occured while fetching all data!");
+    } else {
+      res.send(data);
+    }
+  });
+};
+module.exports.saveUser = (req, res, next) => {
+  console.log(req.body);
+  fs.readFile("data.json", (err, data) => {
+    if (err) {
+      res.send("Error occured while fetching random data!");
+    } else {
+      const allData = JSON.parse(data);
+      allData.push(req.body);
+      fs.writeFile("data.json", JSON.stringify(allData), () => {
+        res.send("New user saved successfully!");
+      });
+    }
+  });
+};
+module.exports.updateUser = (req, res, next) => {
+  res.send("User updated!");
+};
+module.exports.updateMultipleUser = (req, res, next) => {
+  res.send("Updated multiple user!");
+};
+module.exports.deleteUser = (req, res, next) => {
+  res.send("Delete a user!");
+};
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const app=express()
-const port = process.env.PORT||5001;
-const userRouter=require('./routes/v1/user.route')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5001;
+const userRouter = require("./routes/v1/user.route");
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 //apis
-app.use('/api/v1/user',userRouter)
+app.use("/api/v1/user", userRouter);
 //default route
-app.all('*',(req,res)=>{
-    res.send('Invalid route!!!')
-})
+app.all("*", (req, res) => {
+  res.send("Invalid route!!!");
+});
 //home route
-app.get('/',(req,res)=>{
-    res.send('Server is running.')
-})
-app.listen(port,()=>{
-    console.log('server is running from: ',port)
-})
+app.get("/", (req, res) => {
+  res.send("Server is running.");
+});
+app.listen(port, () => {
+  console.log("server is running from: ", port);
+});
